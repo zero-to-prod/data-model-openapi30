7c927c4532abaf00a7c7ec9cbc3eda67f9d86f2d
david_smith <david_smith@sweetwater.com>
2024-11-14 07:28:27 -0500

    Add `ServerTest.php`

--- tests/Unit/Server/ServerTest.php
+++ tests/Unit/Server/ServerTest.php
@@ -13,20 +13,53 @@ class ServerTest extends TestCase
     {
         $json = <<<JSON
         {
-          "url": "https://development.gigantic-server.com/v1",
-          "description": "Development server"
+          "url": "https://{username}.gigantic-server.com:{port}/{basePath}",
+          "description": "The production API server",
+          "variables": {
+            "username": {
+              "default": "demo",
+              "description": "A user-specific subdomain. Use `demo` for a free sandbox environment."
+            },
+            "port": {
+              "enum": ["8443", "443"],
+              "default": "8443"
+            },
+            "basePath": {
+              "default": "v2"
+            }
+          }
         }
         JSON;
 
         $Server = Server::from(json_decode($json, true));
 
         self::assertEquals(
-            expected: 'https://development.gigantic-server.com/v1',
+            expected: 'https://{username}.gigantic-server.com:{port}/{basePath}',
             actual: $Server->url,
         );
         self::assertEquals(
-            expected: 'Development server',
+            expected: 'The production API server',
             actual: $Server->description,
+        );
+        self::assertEquals(
+            expected: 'demo',
+            actual: $Server->variables['username']->default,
+        );
+        self::assertEquals(
+            expected: 'A user-specific subdomain. Use `demo` for a free sandbox environment.',
+            actual: $Server->variables['username']->description,
+        );
+        self::assertEquals(
+            expected: ["8443", "443"],
+            actual: $Server->variables['port']->enum,
+        );
+        self::assertEquals(
+            expected: '8443',
+            actual: $Server->variables['port']->default,
+        );
+        self::assertEquals(
+            expected: 'v2',
+            actual: $Server->variables['basePath']->default,
         );
     }
 }
