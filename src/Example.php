ce77518d79fe3c4638d40a8ee3373823e6ebb94d
david_smith <david_smith@sweetwater.com>
2024-11-24 18:27:38 -0500

    feat/update-allOf-oneOf-anyOf Updated `src/Example.php`.

--- src/Example.php
+++ src/Example.php
@@ -25,6 +25,7 @@ class Example
      * Short description for the example.
      *
      * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
+     * @see  $summary
      */
     public const summary = 'summary';
 
@@ -41,6 +42,7 @@ class Example
      * used for rich text representation.
      *
      * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
+     * @see  $description
      */
     public const description = 'description';
 
@@ -60,6 +62,7 @@ class Example
      * to contain the example, escaping where necessary.
      *
      * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
+     * @see  $value
      */
     public const value = 'value';
 
@@ -74,6 +77,15 @@ class Example
     #[Describe(['cast' => [self::class, 'value']])]
     public mixed $value;
 
+    /**
+     * Embedded literal example. The `value` field and `externalValue` field
+     * are mutually exclusive. To represent examples of media types that
+     * cannot naturally represented in JSON or YAML, use a string value
+     * to contain the example, escaping where necessary.
+     *
+     * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
+     * @see  $value
+     */
     public static function value($value, array $context)
     {
         if (isset($context[self::value], $context[self::externalValue])) {
@@ -90,6 +102,7 @@ class Example
      * exclusive. See the rules for resolving Relative References.
      *
      * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
+     * @see  $externalValue
      */
     public const externalValue = 'externalValue';
 
@@ -104,6 +117,15 @@ class Example
     #[Describe(['cast' => [self::class, 'externalValue']])]
     public ?string $externalValue;
 
+    /**
+     * A URL that points to the literal example. This provides the capability
+     * to reference examples that cannot easily be included in JSON or YAML
+     * documents. The `value` field and `externalValue` field are mutually
+     * exclusive. See the rules for resolving Relative References.
+     *
+     * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
+     * @see  $externalValue
+     */
     public static function externalValue($value, array $context)
     {
         if (isset($context[self::value], $context[self::externalValue])) {
