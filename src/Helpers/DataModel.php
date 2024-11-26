ff986097309fc336c2cad9351407c90639297b1b
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:31 -0500

    feat/remove-exceptions Updated `src/Helpers/DataModel.php`.

--- src/Helpers/DataModel.php
+++ src/Helpers/DataModel.php
@@ -2,10 +2,7 @@
 
 namespace Zerotoprod\DataModelOpenapi30\Helpers;
 
-use ReflectionAttribute;
-use ReflectionProperty;
 use Zerotoprod\DataModelHelper\DataModelHelper;
-use Zerotoprod\DataModelOpenapi30\InvalidMultipleException;
 use Zerotoprod\Transformable\Transformable;
 
 trait DataModel
@@ -13,11 +10,4 @@ trait DataModel
     use \Zerotoprod\DataModel\DataModel;
     use Transformable;
     use DataModelHelper;
-
-    public static function throwException(mixed $value, array $context, ?ReflectionAttribute $Attribute, ReflectionProperty $Property): string|int|null|float
-    {
-        $args = $Attribute?->getArguments()[0];
-
-        throw new ($args['exception'])($args['message'] ?? '');
-    }
 }
