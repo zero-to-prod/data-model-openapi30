41de6cb6985a8c748a681710007a059c5c3b3fde
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:34 -0500

    feat/remove-exceptions Updated `tests/Unit/Schema/MaxPropertiesTest.php`.

--- tests/Unit/Schema/MaxPropertiesTest.php
+++ tests/Unit/Schema/MaxPropertiesTest.php
@@ -4,8 +4,6 @@ namespace Tests\Unit\Schema;
 
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
-use Zerotoprod\DataModelOpenapi30\InvalidMaxItemsException;
-use Zerotoprod\DataModelOpenapi30\InvalidMaxPropertiesException;
 use Zerotoprod\DataModelOpenapi30\Schema;
 
 class MaxPropertiesTest extends TestCase
@@ -57,15 +55,5 @@ class MaxPropertiesTest extends TestCase
             expected: 0,
             actual: $Schema->maxProperties,
         );
-    }
-
-    /** @link https://spec.openapis.org/oas/v3.0.4.html#json-schema-keywords */
-    #[Test] public function non_zero(): void
-    {
-        $this->expectException(InvalidMaxPropertiesException::class);
-
-        Schema::from([
-            Schema::maxProperties => -1,
-        ]);
     }
 }
