4816b827b4a1f857d367c734ca4533114828aea3
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:35 -0500

    feat/remove-exceptions Updated `tests/Unit/Schema/ReadOnlyTest.php`.

--- tests/Unit/Schema/ReadOnlyTest.php
+++ tests/Unit/Schema/ReadOnlyTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit\Schema;
 
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
-use Zerotoprod\DataModelOpenapi30\InvalidReadAndWriteOnlyException;
 use Zerotoprod\DataModelOpenapi30\Schema;
 
 class ReadOnlyTest extends TestCase
@@ -47,26 +46,5 @@ class ReadOnlyTest extends TestCase
             condition: $Schema->readOnly,
             message: 'Default value is false.'
         );
-    }
-
-    /**
-     * Relevant only for Schema Object `properties` definitions. Declares the
-     * property as “read only”. This means that it _MAY_ be sent as part
-     * of a response but _SHOULD_NOT_ be sent as part of the request.
-     * If the property is marked as `readOnly` being `true` and is in
-     * the `required` list, the `required` will take effect on the
-     * response only. A property _MUST NOT_ be marked as both
-     * `readOnly` and `writeOnly` being `true`. Default value
-     * is `false`.
-     *
-     * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-20
-     */
-    #[Test] public function read_and_write_only_cannot_both_be_true(): void
-    {
-        $this->expectException(InvalidReadAndWriteOnlyException::class);
-        Schema::from([
-            Schema::readOnly => true,
-            Schema::writeOnly => true,
-        ]);
     }
 }
