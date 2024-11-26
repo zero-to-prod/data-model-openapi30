4df73c5cad1b3f34e6a158986f73d1fdb346b728
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:35 -0500

    feat/remove-exceptions Updated `tests/Unit/SecurityScheme/TypeTest.php`.

--- tests/Unit/SecurityScheme/TypeTest.php
+++ tests/Unit/SecurityScheme/TypeTest.php
@@ -4,40 +4,10 @@ namespace Tests\Unit\SecurityScheme;
 
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
-use Zerotoprod\DataModel\PropertyRequiredException;
-use Zerotoprod\DataModelOpenapi30\InvalidSecuritySchemeTypeException;
 use Zerotoprod\DataModelOpenapi30\SecurityScheme;
 
 class TypeTest extends TestCase
 {
-
-    /** @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-23 */
-    #[Test] public function default_value(): void
-    {
-        $this->expectException(PropertyRequiredException::class);
-        $this->expectExceptionMessage('Property `$type` is required.');
-
-        SecurityScheme::from([
-            SecurityScheme::name => 'name',
-            SecurityScheme::in => 'query',
-            SecurityScheme::scheme => 'scheme',
-            SecurityScheme::openIdConnectUrl => 'openIdConnectUrl',
-        ]);
-    }
-
-    /** @link @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-23 */
-    #[Test] public function invalid_value(): void
-    {
-        $this->expectException(InvalidSecuritySchemeTypeException::class);
-
-        SecurityScheme::from([
-            SecurityScheme::type => 'bogus',
-            SecurityScheme::name => 'name',
-            SecurityScheme::in => 'query',
-            SecurityScheme::scheme => 'scheme',
-            SecurityScheme::openIdConnectUrl => 'openIdConnectUrl',
-        ]);
-    }
 
     /** @link @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-23 */
     #[Test] public function type(): void
