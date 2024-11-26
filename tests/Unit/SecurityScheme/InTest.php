c8ab065af1f405dddeb75dcf0aa70e050568ceed
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:35 -0500

    feat/remove-exceptions Updated `tests/Unit/SecurityScheme/InTest.php`.

--- tests/Unit/SecurityScheme/InTest.php
+++ tests/Unit/SecurityScheme/InTest.php
@@ -4,9 +4,6 @@ namespace Tests\Unit\SecurityScheme;
 
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
-use Zerotoprod\DataModel\PropertyRequiredException;
-use Zerotoprod\DataModelOpenapi30\InvalidSecuritySchemeInException;
-use Zerotoprod\DataModelOpenapi30\InvalidSecuritySchemeTypeException;
 use Zerotoprod\DataModelOpenapi30\SecurityScheme;
 
 class InTest extends TestCase
@@ -26,20 +23,6 @@ class InTest extends TestCase
             actual: $SecurityScheme->in,
             message: 'REQUIRED. The location of the API key. Valid values are "query", "header", or "cookie".'
         );
-    }
-
-    /** @link @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-23 */
-    #[Test] public function invalid_value(): void
-    {
-        $this->expectException(InvalidSecuritySchemeInException::class);
-
-        SecurityScheme::from([
-            SecurityScheme::type => 'apiKey',
-            SecurityScheme::name => 'name',
-            SecurityScheme::in => 'bogus',
-            SecurityScheme::scheme => 'scheme',
-            SecurityScheme::openIdConnectUrl => 'openIdConnectUrl',
-        ]);
     }
 
     /** @link @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-23 */
