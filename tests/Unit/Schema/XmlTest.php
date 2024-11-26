027756c8a6049864f1ad6a952478f6f296424c82
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:35 -0500

    feat/remove-exceptions Updated `tests/Unit/Schema/XmlTest.php`.

--- tests/Unit/Schema/XmlTest.php
+++ tests/Unit/Schema/XmlTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit\Schema;
 
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
-use Zerotoprod\DataModelOpenapi30\Discriminator;
 use Zerotoprod\DataModelOpenapi30\Schema;
 use Zerotoprod\DataModelOpenapi30\Xml;
 
