784b444a5251f5d56a5b2f01fabe1889ae52c6fe
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:30 -0500

    feat/remove-exceptions Updated `src/Example.php`.

--- src/Example.php
+++ src/Example.php
@@ -74,26 +74,8 @@ class Example
      *
      * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
      */
-    #[Describe(['cast' => [self::class, 'value']])]
+    #[Describe(['nullable'])]
     public mixed $value;
-
-    /**
-     * Embedded literal example. The `value` field and `externalValue` field
-     * are mutually exclusive. To represent examples of media types that
-     * cannot naturally represented in JSON or YAML, use a string value
-     * to contain the example, escaping where necessary.
-     *
-     * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
-     * @see  $value
-     */
-    public static function value($value, array $context)
-    {
-        if (isset($context[self::value], $context[self::externalValue])) {
-            throw new MustBeExclusiveException('$value and $externalValue must be mutually exclusive.');
-        }
-
-        return $context[self::value] ?? null;
-    }
 
     /**
      * A URL that points to the literal example. This provides the capability
@@ -114,24 +96,6 @@ class Example
      *
      * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
      */
-    #[Describe(['cast' => [self::class, 'externalValue']])]
+    #[Describe(['nullable'])]
     public ?string $externalValue;
-
-    /**
-     * A URL that points to the literal example. This provides the capability
-     * to reference examples that cannot easily be included in JSON or YAML
-     * documents. The `value` field and `externalValue` field are mutually
-     * exclusive. See the rules for resolving Relative References.
-     *
-     * @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-15
-     * @see  $externalValue
-     */
-    public static function externalValue($value, array $context)
-    {
-        if (isset($context[self::value], $context[self::externalValue])) {
-            throw new MustBeExclusiveException('$value and $externalValue must be mutually exclusive.');
-        }
-
-        return $context[self::externalValue] ?? null;
-    }
 }
