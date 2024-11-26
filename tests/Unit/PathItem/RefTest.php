ab5f4a5aa01d744a3bfffc6e16bc43481e1930a5
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:34 -0500

    feat/remove-exceptions Updated `tests/Unit/PathItem/RefTest.php`.

--- tests/Unit/PathItem/RefTest.php
+++ tests/Unit/PathItem/RefTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit\PathItem;
 
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
-use Zerotoprod\DataModel\PropertyRequiredException;
 use Zerotoprod\DataModelOpenapi30\PathItem;
 
 class RefTest extends TestCase
