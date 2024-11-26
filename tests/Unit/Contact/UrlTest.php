5609753fa9a2e6e237090be034df06d6a79b6258
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:32 -0500

    feat/remove-exceptions Updated `tests/Unit/Contact/UrlTest.php`.

--- tests/Unit/Contact/UrlTest.php
+++ tests/Unit/Contact/UrlTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit\Contact;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 use Zerotoprod\DataModelOpenapi30\Contact;
-use Zerotoprod\DataModelOpenapi30\InvalidUrlException;
 
 class UrlTest extends TestCase
 {
@@ -18,20 +17,6 @@ class UrlTest extends TestCase
             actual: $Contact->url,
             message: 'The URL for the contact information. This _MUST_ be in the form of a URL.'
         );
-    }
-
-    /**
-     * The URL for the contact information. This _MUST_ be in the form of a URL.
-     *
-     * @link https://spec.openapis.org/oas/v3.0.4.html#contact-object
-     */
-    #[Test] public function invalid_url(): void
-    {
-        $this->expectException(InvalidUrlException::class);
-
-        Contact::from([
-            Contact::url => 'invalid url'
-        ]);
     }
 
     /** @link https://spec.openapis.org/oas/v3.0.4.html#contact-object */
