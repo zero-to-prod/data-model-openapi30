2f8e0ae3fd33d500dac3e02052ee207cfc87581f
david_smith <david_smith@sweetwater.com>
2024-11-25 21:25:33 -0500

    feat/remove-exceptions Updated `tests/Unit/Info/TermsOfServiceTest.php`.

--- tests/Unit/Info/TermsOfServiceTest.php
+++ tests/Unit/Info/TermsOfServiceTest.php
@@ -6,25 +6,9 @@ use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 use Zerotoprod\DataModel\PropertyRequiredException;
 use Zerotoprod\DataModelOpenapi30\Info;
-use Zerotoprod\DataModelOpenapi30\InvalidUrlException;
 
 class TermsOfServiceTest extends TestCase
 {
-    /**
-     * A URL for the Terms of Service for the API. This _MUST_ be in the form of a URL.
-     *
-     * link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-0
-     */
-    #[Test] public function invalid_termsOfService(): void
-    {
-        $this->expectException(InvalidUrlException::class);
-
-        Info::from([
-            Info::title => 'title',
-            Info::termsOfService => 'termsOfService',
-            Info::version => 'version',
-        ]);
-    }
 
     /** @link https://spec.openapis.org/oas/v3.0.4.html#fixed-fields-0 */
     #[Test] public function termsOfService(): void
